e2a01e446e901b93060da8d0a30a9e077ce8d06a
padames@XP13 <pablo.adames@protonmail.com>
2024-11-12 23:00:20 -0700

    Made clear critically-damped and under-damped solutions not implemented yet

--- R/compute_particular_solution.R
+++ R/compute_particular_solution.R
@@ -32,10 +32,12 @@ create_particular_solution_function_fn <- function(mass, damping, stiffness, inp
   }
   else if (poles_type == pole_types$TWO_COMPLEX_CONJUGATE)
   {
+    stop("Under-damped solution not implemeted yet")
     create_particular_function_roots_complex_conjugate_fn(mass, damping, stiffness, input_force, frequency)
   }
   else
   {
+    stop("Critically damped solution not implemneted yet")
     create_particular_function_roots_real_repeated_fn(mass, damping, stiffness, input_force, frequency)
   }
   
@@ -92,42 +94,55 @@ create_particular_function_roots_real_distintc_fn <- function(mass, damping, sti
 }
 
 
-create_particular_function_roots_complex_conjugate_fn <- function()
+create_particular_function_roots_complex_conjugate_fn <- function(mass, damping, stiffness, input_force, frequency)
 {
-  two_complex_conjugate_poles <- compute_two_complex_conjugate_poles_fn(mass, damping, stiffness)
-  
-  p1 <- two_complex_conjugate_poles$p1
-  p2 <- two_complex_conjugate_poles$p2
-  
-  q_num <- input_force * frequency
-  
-  q1_den <- (p1*p1 + frequency*frequency) * (p1 - p2)
-  q2_den <- (p2*p2 + frequency*frequency) * (p2 - p1)
-  
-  q1 <- q_num / q1_den
-  q2 <- q_num / q2_den
-  
-  real_part <- Re(p1)
-  imaginary_part <- Im(p1)
-  
-  exp_term <- exp(real_part)
-  
-  q1_plus_q2 <- q1 + q2
-  q1_minus_q2 <- q1 - q2
-  
-  function(seconds_to_simulate)
+  if (FALSE)
   {
-    num_time_points_to_simulate <- length(seconds_to_simulate)
-    # compute all vectors
-    q1_plus_q2_s <- rep(q1_plus_q2, times = num_time_points_to_simulate)
-    q1_minus_q2_s <- rep(q1_minus_q2, times = num_time_points_to_simulate)
-    exp_term_s <- rep(exp_term, times = num_time_points_to_simulate)
-    imaginary_part_s <- rep(imaginary_part, times = num_time_points_to_simulate)
+    # this won't work because the real and imaginary parts have to
+    # be separated and the wave solution be formed then.
+    two_complex_conjugate_poles <- compute_two_complex_conjugate_poles_fn(mass, damping, stiffness)
     
-    # now compose the particular response to the under-damped case
-    exp_term_s * ( q1_plus_q2_s * cos(imaginary_part_s * seconds_to_simulate ) + 
-                   q1_minus_q2_s * sin( imaginary_part_s * seconds_to_simulate))
-  }  
+    p1 <- two_complex_conjugate_poles$p1
+    p2 <- two_complex_conjugate_poles$p2
+    
+    q_num <- input_force * frequency
+    
+    q1_den <- (p1*p1 + frequency*frequency) * (p1 - p2)
+    q2_den <- (p2*p2 + frequency*frequency) * (p2 - p1)
+    
+    q1 <- q_num / q1_den
+    q2 <- q_num / q2_den
+    
+    real_part <- Re(p1)
+    imaginary_part <- Im(p1)
+    
+    exp_term <- exp(real_part)
+    
+    q1_plus_q2 <- q1 + q2
+    q1_minus_q2 <- q1 - q2
+    
+    function(seconds_to_simulate)
+    {
+      num_time_points_to_simulate <- length(seconds_to_simulate)
+      # compute all vectors
+      q1_plus_q2_s <- rep(q1_plus_q2, times = num_time_points_to_simulate)
+      q1_minus_q2_s <- rep(q1_minus_q2, times = num_time_points_to_simulate)
+      exp_term_s <- rep(exp_term, times = num_time_points_to_simulate)
+      imaginary_part_s <- rep(imaginary_part, times = num_time_points_to_simulate)
+      
+      # now compose the particular response to the under-damped case
+      exp_term_s * ( q1_plus_q2_s * cos(imaginary_part_s * seconds_to_simulate ) + 
+                     q1_minus_q2_s * sin( imaginary_part_s * seconds_to_simulate))
+    }  
+  }
+  # TODO: replace this stub for the real function
+  
+  function(vector_of_times)
+  {
+    num_time_points_to_simulate <- length(vector_of_times)
+    rep(0, times = num_time_points_to_simulate)
+  }
+  
 }
 
 
